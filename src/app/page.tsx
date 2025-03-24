947377264fc4682346c12bb9fb6e63540c2b9b36
tkkxttys <kittycatty.2548@gmail.com>
2025-03-24 21:31:02 +0700

    decor app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+/*import Image from "next/image";
 import Banner from '@/components/Banner';
 import Card from '@/components/Card'
 import styles from "./page.module.css";
@@ -11,6 +11,18 @@ export default function Home() {
     <main>
       <Banner/>
       <TravelCard></TravelCard>
+    </main>
+  );
+}*/
+
+import Banner from '@/components/Banner';
+import { TravelCard } from "@/components/TravelCard";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center w-full max-w-screen-lg mx-auto gap-6">
+      <Banner />
+      <TravelCard />
     </main>
   );
 }
