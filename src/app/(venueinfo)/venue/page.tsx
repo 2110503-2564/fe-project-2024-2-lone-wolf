369f588b94cc69a7a61de96eaae22b9bec40d15a
tkkxttys <kittycatty.2548@gmail.com>
2025-03-24 21:45:41 +0700

    decor [vid] venue page

--- src/app/(venueinfo)/venue/page.tsx
+++ src/app/(venueinfo)/venue/page.tsx
@@ -1,4 +1,4 @@
-import CardPanel from "@/components/CardPanel"
+/*import CardPanel from "@/components/CardPanel"
 import Card from "@/components/InteractiveCard"
 import getVenues from "@/libs/getVenues";
 import { VenueJson } from "../../../../interface"
@@ -18,6 +18,29 @@ export default async function Venue(){
                 <h1 className="text-xl font-medium">TRY Client-Side Hotel</h1>
                 <CardPanel/>
             </Suspense>
+        </main>
+    )
+}*/
+
+import CardPanel from "@/components/CardPanel"
+import getVenues from "@/libs/getVenues";
+import { VenueJson } from "../../../../interface"
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
+import VenueCatalog from "@/components/VenueCatalog";
+
+export default async function Venue() {
+    const venues: VenueJson = await getVenues();
+
+    return (
+        <main className="p-5">
+            <h1 className="text-xl font-medium text-gray-700 mb-6">Select Your Hotel</h1>
+            <Suspense fallback={<div className="flex justify-center items-center py-10"><LinearProgress className="w-full" /></div>}>
+                <VenueCatalog venuesJson={venues} />
+            </Suspense>
+            <hr className="my-10 border-t border-gray-300" />
+            <h1 className="text-xl font-medium text-gray-700 mb-6">TRY Client-Side Hotel</h1>
+            <CardPanel />
         </main>
     )
 }
