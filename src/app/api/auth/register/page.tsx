042a8a84f9084ae9a2ba4e885d7ec1f076bcbbec
tkkxttys <kittycatty.2548@gmail.com>
2025-03-24 21:47:00 +0700

    decor register page

--- src/app/api/auth/register/page.tsx
+++ src/app/api/auth/register/page.tsx
@@ -1,5 +1,5 @@
 // components/Register.tsx or pages/register.tsx in Next.js
-'use client'
+/*'use client'
 import { User } from "../../../../../interface";
 import userRegister from "@/libs/userRegister";
 import { useState } from "react";
@@ -72,5 +72,94 @@ export default function Register() {
                 shadow-sm text-white" name="Sign Up" onClick={makeUser}>Sign Up</button>
             </main>
   );
+};*/
+
+'use client'
+import { User } from "../../../../../interface";
+import userRegister from "@/libs/userRegister";
+import { useState } from "react";
+import { TextField } from "@mui/material";
+
+export default function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [tel, setTel] = useState("");
+
+  const makeUser = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const user: User = {
+        name,
+        email,
+        password,
+        tel
+    }
+    
+    const response = await userRegister(user)
+
+    if(response.ok)
+      console.log('user created')
+  };
+
+  return (
+    <main className="w-full max-w-md mx-auto p-6 space-y-4 bg-white rounded-lg shadow-lg">
+      <h2 className="text-2xl font-semibold text-center text-gray-800">Sign Up</h2>
+      <form className="space-y-4" onSubmit={makeUser}>
+        <div className="space-y-1">
+          <label className="text-md font-medium text-gray-700">Name</label>
+          <TextField
+            className="w-full"
+            variant="outlined"
+            fullWidth
+            name="Name-Lastname"
+            onChange={e => setName(e.target.value)}  
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-md font-medium text-gray-700">Email</label>
+          <TextField
+            className="w-full"
+            variant="outlined"
+            fullWidth
+            name="Email"
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-md font-medium text-gray-700">Contact Number</label>
+          <TextField
+            className="w-full"
+            variant="outlined"
+            fullWidth
+            name="Contact-Number"
+            onChange={e => setTel(e.target.value)}
+          />
+        </div>
+
+        <div className="space-y-1">
+          <label className="text-md font-medium text-gray-700">Password</label>
+          <TextField
+            className="w-full"
+            variant="outlined"
+            fullWidth
+            name="Password"
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 bg-sky-600 text-white font-medium rounded-md shadow-md hover:bg-indigo-600 transition duration-300"
+        >
+          Sign Up
+        </button>
+      </form>
+    </main>
+  );
 };
+
 
